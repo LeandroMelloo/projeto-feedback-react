d473554936d02bab6f9810f497ab1a75d26aef09
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-05-05 21:22:11 -0300

    criando componente de feedback

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.png";
+import ideaImageUrl from "../../assets/idea.png";
+import thoughtImageUrl from "../../assets/thought.png";
+
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um notebook com um ponto de exclamação",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} /> // comunicação entre componentes
+      ) : (
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito por{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://github.com/LeandroMelloo"
+        >
+          Leandro Mello
+        </a>
+      </footer>
+    </div>
+  );
+}
